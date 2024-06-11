a6b800c11fc9eb2df972ed353eb21041281236b8
Aaron Orenstein <aorenste@fb.com>
2024-06-10 20:43:53 -0700

    fake_tensor.py: annotate types
    ghstack-source-id: 644fe1de1b0da80051faad599bd0f6c1e0f3164a
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/128041

--- torch/types.py
+++ torch/types.py
@@ -3,6 +3,7 @@ import builtins
 from typing import Any, List, Optional, Sequence, Tuple, Union
 
 import torch
+from torch import SymInt
 
 # Convenience aliases for common composite types that we need
 # to talk about in PyTorch
@@ -28,6 +29,7 @@ _dtype = torch.dtype
 _device = torch.device
 _qscheme = torch.qscheme
 _size = Union[torch.Size, List[_int], Tuple[_int, ...]]
+_symsize = Union[torch.Size, List[Union[_int, SymInt]], Tuple[Union[_int, SymInt], ...]]
 _layout = torch.layout
 _dispatchkey = Union[str, torch._C.DispatchKey]
 
